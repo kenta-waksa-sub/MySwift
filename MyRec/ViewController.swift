8e7866fa5fe48acd1560935fd7ef8ed99a6cb3df
konbukonbu815 <konbukonbu815@gmail.com>
2017-07-11 14:39:21 +0900

    録音、停止、再生ができる

--- MyRec/ViewController.swift
+++ MyRec/ViewController.swift
@@ -14,13 +14,11 @@ class ViewController: UIViewController {
     let fileManager = FileManager()
     var audioRecorder: AVAudioRecorder?
     var audioPlayer: AVAudioPlayer?
-    let fileName = "sample.caf"
-     //documentディレクトリのURLを取得
-    let documentDirFileURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).last!
     
     
     @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,6 +31,11 @@ class ViewController: UIViewController {
     @IBAction func pushRecordButton(_ sender: Any) {
         audioRecorder?.record()
     }
+    
+    @IBAction func pushStopButton(_ sender: Any) {
+        audioRecorder?.stop()
+    }
+    
     @IBAction func pushPlayButton(_ sender: Any) {
         self.play()
     }
@@ -42,21 +45,22 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
     //　録音のための初期設定
     func setupAudioRecorder(){
         //　再生と録音機能をアクティブにする
         let session = AVAudioSession.sharedInstance()
         try! session.setCategory(AVAudioSessionCategoryPlayAndRecord)
         try! session.setActive(true)
-        let recordSetting : [String : AnyObject] = [
-            AVEncoderAudioQualityKey : AVAudioQuality.min.rawValue as AnyObject,
-            AVEncoderBitRateKey : 16 as AnyObject,
-            AVNumberOfChannelsKey : 2 as AnyObject,
-            AVSampleRateKey: 44100.0 as AnyObject
+        let recordSetting : [String : Any] = [
+            AVEncoderAudioQualityKey : AVAudioQuality.min.rawValue,
+            AVEncoderBitRateKey : 16,
+            AVNumberOfChannelsKey : 2,
+            AVSampleRateKey: 44100.0
         ]
         do {
-            try audioRecorder = AVAudioRecorder(url: documentDirFileURL as URL, settings: recordSetting)
+            try audioRecorder = AVAudioRecorder(url: self.documentFilePath(), settings: recordSetting)
         } catch {
             print("初期設定時にerror")
         }
@@ -65,13 +69,21 @@ class ViewController: UIViewController {
     // 再生
     func play(){
         do {
-            try audioPlayer = AVAudioPlayer(contentsOf: documentDirFileURL)
+            try audioPlayer = AVAudioPlayer(contentsOf: self.documentFilePath())
         }catch {
-            print("再生時にerror出たよ(´・ω・｀)")
+            print("再生時にerror")
         }
         audioPlayer?.play()
     }
 
+    func documentFilePath()-> URL {
+        // URLの指定
+        let thePath = NSHomeDirectory() + "/Documents/sample.caf"
+        // pathをURLに変更
+        let fileURL = URL(fileURLWithPath: thePath)
+        print(fileURL)
+        return fileURL
+    }
 
 }
 
